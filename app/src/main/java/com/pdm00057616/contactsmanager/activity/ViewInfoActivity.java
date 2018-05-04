eef21ec7b6f8a36ff5aa72ca69fe42bdb3a33abf
petrlr14 <00057616@uca.edu.sv>
2018-05-04 12:36:35 -0600

    se agrego actividad para ver informacion completa de usuarios

---
+++ app/src/main/java/com/pdm00057616/contactsmanager/activity/ViewInfoActivity.java
@@ -0,0 +1,15 @@
+package com.pdm00057616.contactsmanager.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pdm00057616.contactsmanager.R;
+
+public class ViewInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_info);
+    }
+}
