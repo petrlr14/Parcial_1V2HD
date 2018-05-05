062c06ea38e4d0986847b532aa9ebe0bfb6bf5aa
petrlr14 <00057616@uca.edu.sv>
2018-05-05 10:48:01 -0600

    se borro y creo otra clase modelo, para tener mayor inforacion del contacto

---
+++ app/src/main/java/com/pdm00057616/contactsmanager/model/Contacts.kt
@@ -0,0 +1,121 @@
+package com.pdm00057616.contactsmanager.model
+
+import android.graphics.Bitmap
+import android.os.Parcel
+import android.os.Parcelable
+
+class Contacts(var name:String?, var numbers:List<PhoneNumber>?, var email:List<Email>?, @Transient var photo: Bitmap?, var id:String?, var fav:Boolean?, var address:List<Address>?, var birthday:String?) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.createTypedArrayList(PhoneNumber),
+            parcel.createTypedArrayList(Email),
+            parcel.readParcelable(Bitmap::class.java.classLoader),
+            parcel.readString(),
+            parcel.readValue(Boolean::class.java.classLoader) as? Boolean,
+            parcel.createTypedArrayList(Address),
+            parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeTypedList(numbers)
+        parcel.writeTypedList(email)
+        parcel.writeParcelable(photo, flags)
+        parcel.writeString(id)
+        parcel.writeValue(fav)
+        parcel.writeTypedList(address)
+        parcel.writeString(birthday)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Contacts> {
+        override fun createFromParcel(parcel: Parcel): Contacts {
+            return Contacts(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Contacts?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+class PhoneNumber(var number: String?, var type: Int): Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readInt()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(number)
+        parcel.writeInt(type)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PhoneNumber> {
+        override fun createFromParcel(parcel: Parcel): PhoneNumber {
+            return PhoneNumber(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PhoneNumber?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+class Email(var email: String?, var type: Int) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readInt()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(email)
+        parcel.writeInt(type)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Email> {
+        override fun createFromParcel(parcel: Parcel): Email {
+            return Email(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Email?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
+class Address(var address: String?, var type: Int) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readInt()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(address)
+        parcel.writeInt(type)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Address> {
+        override fun createFromParcel(parcel: Parcel): Address {
+            return Address(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Address?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
