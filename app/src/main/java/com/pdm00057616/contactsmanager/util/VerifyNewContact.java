6ce2716f4161686fab2aa9a110aca778efe8fe0a
petrlr14 <00057616@uca.edu.sv>
2018-05-05 23:15:39 -0600

    clase que ayuda a verificar si el contacto esta listo para guardarse

--- app/src/main/java/com/pdm00057616/contactsmanager/util/VerifyNewContact.java
+++ app/src/main/java/com/pdm00057616/contactsmanager/util/VerifyNewContact.java
@@ -1,4 +1,35 @@
 package com.pdm00057616.contactsmanager.util;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.Toast;
+
+import com.pdm00057616.contactsmanager.model.Address;
+import com.pdm00057616.contactsmanager.model.Email;
+import com.pdm00057616.contactsmanager.model.PhoneNumber;
+
+import java.util.ArrayList;
+
 public class VerifyNewContact {
+
+    public static boolean isReady(String name, String birthday, ArrayList<PhoneNumber> phones, ArrayList<Email> emails, ArrayList<Address> addresses, Context context){
+        if(name.equals("")&&birthday.equals("")&&phones.size()==0&&emails.size()==0&&addresses.size()==0){
+            Toast.makeText(context, "debe ingresar almenos nombre y un campo que no sea cumpleaños", Toast.LENGTH_SHORT).show();
+            return false;
+        }else{
+            if(!name.equals("")){
+                System.out.println(phones.size()+"  "+emails.size()+"   "+addresses.size());
+                if(phones.size()>0||emails.size()>0||addresses.size()>0){
+                    return true;
+                }else{
+                    Toast.makeText(context, "Debe ingresar un campo adicional que no sea cumpleaños", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }else{
+                Toast.makeText(context, "Debe ingresar el nombre del contacto", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+    }
+
 }
