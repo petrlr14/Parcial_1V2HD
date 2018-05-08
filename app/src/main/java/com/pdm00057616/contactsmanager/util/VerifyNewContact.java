1622fdac3fe8965fc3136f016c686cc94335d8f1
petrlr14 <00057616@uca.edu.sv>
2018-05-08 02:53:42 -0600

    traduccion, final final commit

--- app/src/main/java/com/pdm00057616/contactsmanager/util/VerifyNewContact.java
+++ app/src/main/java/com/pdm00057616/contactsmanager/util/VerifyNewContact.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.widget.Toast;
 
+import com.pdm00057616.contactsmanager.R;
 import com.pdm00057616.contactsmanager.model.Address;
 import com.pdm00057616.contactsmanager.model.Email;
 import com.pdm00057616.contactsmanager.model.PhoneNumber;
@@ -14,7 +15,7 @@ public class VerifyNewContact {
 
     public static boolean isReady(String name, String birthday, ArrayList<PhoneNumber> phones, ArrayList<Email> emails, ArrayList<Address> addresses, Context context){
         if(name.equals("")&&birthday.equals("")&&phones.size()==0&&emails.size()==0&&addresses.size()==0){
-            Toast.makeText(context, "debe ingresar almenos nombre y un campo que no sea cumpleaños", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, context.getResources().getString(R.string.toast1), Toast.LENGTH_SHORT).show();
             return false;
         }else{
             if(!name.equals("")){
@@ -22,11 +23,11 @@ public class VerifyNewContact {
                 if(phones.size()>0||emails.size()>0||addresses.size()>0){
                     return true;
                 }else{
-                    Toast.makeText(context, "Debe ingresar un campo adicional que no sea cumpleaños", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, context.getResources().getString(R.string.toast2), Toast.LENGTH_SHORT).show();
                     return false;
                 }
             }else{
-                Toast.makeText(context, "Debe ingresar el nombre del contacto", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, context.getResources().getString(R.string.toast3), Toast.LENGTH_SHORT).show();
                 return false;
             }
         }
